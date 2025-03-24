5726373d690da05b957946393f4b8567b42a4543
Jannik Baranczyk <8103530+jatnnik@users.noreply.github.com>
2025-03-24 15:36:59 +0100

    extend AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+        $this->configureCommands();
+        $this->configureDates();
+    }
+
+    /**
+     * Configure the applications models.
+     */
+    protected function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    /**
+     * Configure the applications commands.
+     */
+    protected function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    /**
+     * Configure the applications dates (use immutable dates by default).
+     * Note: Remember to use the `Date` facade instead of `Carbon`.
+     */
+    protected function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
